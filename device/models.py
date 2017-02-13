76eb49b90e605d3880083f75be63766b906335b0
Ravikiran Reddy K <kravikiran@ssdi.sharp.co.in>
2017-02-13 16:49:58 +0530

    Display filed name instead of Object

--- device/models.py
+++ device/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+class DeviceType(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.CharField(max_length=100)
+    
+    def __unicode__(self):
+        return self.name
+
+class Device(models.Model):
+    """
+    A model which holds information about a particular location
+    """
+    type = models.ForeignKey(DeviceType)
+    name = models.CharField(max_length=255)
+    desc = models.CharField(max_length=100)
+    message = models.CharField(max_length=100)
+    location = models.PointField()
+
+    def __unicode__(self):
+        return self.name
