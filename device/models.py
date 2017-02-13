5fd6b6b9fe29494e5921bf00950e962e09c17073
Ravikiran Reddy K <kravikiran@ssdi.sharp.co.in>
2017-02-13 16:54:51 +0530

    Allow read-only access for unauthenticated users

--- device/models.py
+++ device/models.py
@@ -8,7 +8,7 @@ from django.contrib.gis.db import models
 class DeviceType(models.Model):
     name = models.CharField(max_length=255)
     desc = models.CharField(max_length=100)
-    
+
     def __unicode__(self):
         return self.name
 
