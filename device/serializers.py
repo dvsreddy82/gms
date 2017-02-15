62bc68e37c33757f83e437f21fe60323ffa59791
Ravikiran Reddy K <kravikiran@ssdi.sharp.co.in>
2017-02-15 14:30:11 +0530

    Add GeoFeatureModelSerializer

--- device/serializers.py
+++ device/serializers.py
@@ -1,6 +1,7 @@
 from .models import Device,DeviceType, LANGUAGE_CHOICES, STYLE_CHOICES
 from rest_framework import pagination, serializers
 from django.contrib.auth.models import User, Group
+from rest_framework_gis import serializers as gis_serializers
 
 class DeviceTypeSerializer(serializers.HyperlinkedModelSerializer):
     """ A class to serialize locations as GeoJSON compatible data """
@@ -11,7 +12,7 @@ class DeviceTypeSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'name', 'desc')
 
 
-class DeviceSerializer(serializers.HyperlinkedModelSerializer):
+class DeviceSerializer(gis_serializers.GeoFeatureModelSerializer):
     """ A class to serialize locations as GeoJSON compatible data """
     class Meta:
         model = Device
